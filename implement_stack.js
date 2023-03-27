c7dbf90f82e2bf30efbe56e6e955d71bef22ac37
Aatikahakim <Aatikahakim@gmail.com>
2023-03-27 14:40:32 +0500

    first commit

---
+++ implement_stack.js
@@ -0,0 +1,26 @@
+// Implement a stack using an array as the underlying container.
+
+// The Stack class should support the following methods:
+
+// int size()
+// boolean isEmpty()
+// int top()
+// void push(int element)
+// void pop()
+
+let stack = [];
+
+stack.push(1);
+console.log(stack); // [1]
+
+stack.push(2);
+console.log(stack); // [1,2]
+
+stack.push(3);
+console.log(stack); // [1,2,3]
+
+stack.push(4);
+console.log(stack); // [1,2,3,4]
+
+stack.push(5);
+console.log(stack); // [1,2,3,4,5]
