b7eb2094de6b0f3105631382a7bb0f94c5e9ff88
Aatikahakim <Aatikahakim@gmail.com>
2023-03-29 12:52:04 +0500

    function to add an element x in the stack

--- linked_list_stack.js
+++ linked_list_stack.js
@@ -17,4 +17,17 @@ class LinkedlistStack
 	{
 		this.top=null;
 	}
+
+    // function to add an element x in the stack
+	push(x)
+	{
+		// create new node temp and allocate memory
+		let temp = new Node();
+
+		// check if stack (heap) is full.
+		if (temp == null) {
+			console.log("Heap Overflow");
+			return;
+		}
+    }
 }
