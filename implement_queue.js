c14ee610630381091357de1c4615de73df82e74b
Aatikahakim <Aatikahakim@gmail.com>
2023-03-29 13:20:39 +0500

    Function to insert an element at the next of the queue

---
+++ implement_queue.js
@@ -0,0 +1,30 @@
+// Implement a queue using an array
+
+class Queue{
+    constructor(c)
+    {
+        this.front = this.next = 0; //next point to next block of end ele
+        this.capacity = c;
+        this.queue = new Array(this.capacity);
+    }
+
+    
+// Function to insert an element at the next of the queue
+queueEnqueue(data)
+{
+	// Check queue is full or not
+	if (this.capacity == this.next)
+	{
+		console.log("Queue is full");
+		return;
+	}
+
+	// insert element at the next
+	else
+	{
+		this.queue[this.next] = data;
+		this.next++;
+	}
+	return;
+}
+}
